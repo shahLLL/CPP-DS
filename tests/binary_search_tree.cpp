8caf48aa1502c46da41358b03b3644d959deaa04
shahLLL <shahlll@yahoo.com>
2026-08-02 20:56:09 -0700

    Add binary search tree test cases for remove method
    ISSUE: https://github.com/shahLLL/CPP-DS/issues/13

--- tests/binary_search_tree.cpp
+++ tests/binary_search_tree.cpp
@@ -75,7 +75,6 @@ TEST_CASE("BINARY SEARCH TREE TEST CASE #2", "[binary_search_tree]") {
     REQUIRE(bst.contains(val3));
 }
 
-/* TODO: Finsish adding testcases for remove() method.
 TEST_CASE("BINARY SEARCH TREE TEST CASE #3", "[binary_search_tree]") {
     BinarySearchTree<int> bst = BinarySearchTree<int>();
     for(int i = 0; i < 5; i++) REQUIRE(!bst.remove(i + 1));
@@ -88,5 +87,48 @@ TEST_CASE("BINARY SEARCH TREE TEST CASE #3", "[binary_search_tree]") {
         REQUIRE(bst.isEmpty());
         REQUIRE(bst.getSize() == 0);
     }
+
+    for(int i = 0; i < 5; i++) {
+        bst.add(i + 1);
+        bst.add(i + 2);
+        REQUIRE(bst.getSize() == 2);
+        REQUIRE(bst.contains(i + 1));
+        REQUIRE(bst.contains(i + 2));
+        REQUIRE(bst.remove(i + 1));
+        REQUIRE(bst.getSize() == 1);
+        REQUIRE(!bst.contains(i + 1));
+        REQUIRE(bst.contains(i + 2));
+        REQUIRE(bst.remove(i + 2));
+    }
+
+    for(int i = 0; i < 5; i++) {
+        bst.add(i + 2);
+        bst.add(i + 1);
+        REQUIRE(bst.getSize() == 2);
+        REQUIRE(bst.contains(i + 2));
+        REQUIRE(bst.contains(i + 1));
+        REQUIRE(bst.remove(i + 2));
+        REQUIRE(bst.getSize() == 1);
+        REQUIRE(!bst.contains(i + 2));
+        REQUIRE(bst.contains(i + 1));
+        REQUIRE(bst.remove(i + 1));
+    }
+
+    for(int i = 0; i < 5; i++) {
+        bst.add(i + 2);
+        bst.add(i + 1);
+        bst.add(i + 3);
+        REQUIRE(bst.getSize() == 3);
+        REQUIRE(bst.contains(i + 2));
+        REQUIRE(bst.contains(i + 1));
+        REQUIRE(bst.contains(i + 3));
+        REQUIRE(bst.remove(i + 2));
+        REQUIRE(bst.getSize() == 2);
+        REQUIRE(!bst.contains(i + 2));
+        REQUIRE(bst.contains(i + 1));
+        REQUIRE(bst.contains(i + 3));
+        REQUIRE(bst.remove(i + 1));
+        REQUIRE(bst.remove(i + 3));
+    }
     
-}*/
+}
