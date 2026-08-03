d5f4d50eb0b46b0d29aa0c5958fc3b7bf7270d89
shahLLL <shahlll@yahoo.com>
2026-08-02 20:46:51 -0700

    Add binary search tree testcases for double type
    ISSUE: https://github.com/shahLLL/CPP-DS/issues/13

--- tests/binary_search_tree.cpp
+++ tests/binary_search_tree.cpp
@@ -36,5 +36,57 @@ TEST_CASE("BINARY SEARCH TREE TEST CASE #1", "[binary_search_tree]") {
     REQUIRE(bst.contains(val1));
     REQUIRE(bst.contains(val2));
     REQUIRE(bst.contains(val3));
-
 }
+
+TEST_CASE("BINARY SEARCH TREE TEST CASE #2", "[binary_search_tree]") {
+    double val1 = 2.22;
+    double val2 = 1.11;
+    double val3 = 3.33;
+    BinarySearchTree<double> bst = BinarySearchTree<double>();
+    REQUIRE(bst.isEmpty());
+    REQUIRE(bst.getSize() == 0);
+
+    REQUIRE(!bst.contains(val1));
+    REQUIRE(!bst.contains(val2));
+    REQUIRE(!bst.contains(val3));
+
+    REQUIRE(bst.add(val1));
+
+    REQUIRE(!bst.isEmpty());
+    REQUIRE(bst.getSize() == 1);
+    REQUIRE(bst.contains(val1));
+    REQUIRE(!bst.contains(val2));
+    REQUIRE(!bst.contains(val3));
+
+    REQUIRE(bst.add(val2));
+
+    REQUIRE(!bst.isEmpty());
+    REQUIRE(bst.getSize() == 2);
+    REQUIRE(bst.contains(val1));
+    REQUIRE(bst.contains(val2));
+    REQUIRE(!bst.contains(val3));
+
+    REQUIRE(bst.add(val3));
+
+    REQUIRE(!bst.isEmpty());
+    REQUIRE(bst.getSize() == 3);
+    REQUIRE(bst.contains(val1));
+    REQUIRE(bst.contains(val2));
+    REQUIRE(bst.contains(val3));
+}
+
+/* TODO: Finsish adding testcases for remove() method.
+TEST_CASE("BINARY SEARCH TREE TEST CASE #3", "[binary_search_tree]") {
+    BinarySearchTree<int> bst = BinarySearchTree<int>();
+    for(int i = 0; i < 5; i++) REQUIRE(!bst.remove(i + 1));
+
+    for(int i = 0; i < 5; i++) {
+        bst.add(i + 1);
+        REQUIRE(bst.getSize() == 1);
+        REQUIRE(!bst.isEmpty());
+        REQUIRE(bst.remove(i + 1));
+        REQUIRE(bst.isEmpty());
+        REQUIRE(bst.getSize() == 0);
+    }
+    
+}*/
